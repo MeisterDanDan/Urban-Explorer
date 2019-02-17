814daeef0bf6c7b8368ab1219cbc6909d8f1c7ea
niklas <niklasfichtner1@live.de>
2019-02-17 20:55:57 +0100

    firebase_init

---
+++ Code/index.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAlsu8WQwzfgwHy8k--CnnaXO2xYZ-B0yc",
+    authDomain: "urban-explorer-f661d.firebaseapp.com",
+    databaseURL: "https://urban-explorer-f661d.firebaseio.com",
+    projectId: "urban-explorer-f661d",
+    storageBucket: "urban-explorer-f661d.appspot.com",
+    messagingSenderId: "683954933321"
+  };
+  firebase.initializeApp(config);
+  
+var db = firebase.firestore();
+const firestore = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
