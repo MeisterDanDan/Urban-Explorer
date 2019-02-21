4014411cb5f75393c19ce24283dc893a3ea9f12b
niklas <niklasfichtner1@live.de>
2019-02-21 17:38:05 +0100

    random zitate

--- Code/index.js
+++ Code/index.js
@@ -9,16 +9,33 @@
   };
   firebase.initializeApp(config);
 
-  var db = firebase.firestore();
-  const firestore = firebase.firestore();
-  const settings = {timestampsInSnapshots: true};
-  firestore.settings(settings);
+var db = firebase.firestore();
 
-//get zitat
-db.collection("Zitate").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        const list_div = document.getElementById("commit");
-        var info ="<h3><mark>"+doc.data().Zitat+"</mark></h3><em><mark>"+doc.data().Autor+"</mark></em>";
-        list_div.innerHTML = info
-    });
-});
+
+//random zitate
+      window.addEventListener("DOMContentLoaded", function() {
+          // Create a random var between 1 and 10
+          var randomcom = Math.floor((Math.random() * 10) + 1);
+          // Create an Array with the relative paths of the background pictures
+          var commit = ["error", "<h3>Reisen ist gut</h3><em>- paul</em>", "<h3>Reisen macht Spaß</h3><em>- paul</em>", "<h3>Reisen ist schön</h3><em>- paul</em>", "<h3>jaja</h3><em>- paul</em>", "<h3>neinnein</h3><em>- paul</em>", "<h3>jup</h3><em>- paul</em>", "<h3>lappen</h3><em>- paul</em>", "<h3>commit</h3><em>- paul</em>", "<h3>jojo</h3><em>- paul</em>", "<h3>Die beste Bildung findet ein gescheiter Mensch auf Reisen.</h3><em>- paul</em>"];
+
+          // Set the background with random number and bgIMG Array
+          //document.getElementById('bgID').style.background-image = url("img/paris.jpg");
+
+          document.getElementById("commit").innerHTML = commit[randomcom];
+      }, false);
+
+//bg pictures
+      window.addEventListener("DOMContentLoaded", function() {
+          // Create a random var between 1 and 10
+          var random = Math.floor((Math.random() * 10) + 1);
+          // Create an Array with the relative paths
+          var bgIMG = ["error", "img/bg/amsterdam.jpg", "img/bg/paris.jpg", "img/bg/rom.jpg", "img/bg/berlin.jpg", "img/bg/newyork.jpg", "img/bg/barcelona.jpg", "img/bg/singapur.jpg", "img/bg/porto.jpg", "img/bg/losangeles.jpg", "img/bg/tokyo.jpg"];
+          // Create the var for setting the background path
+          var bg = "url(\'" + bgIMG[random]+"\')";
+
+          // Set the background with random number and bgIMG Array
+          document.body.style.backgroundImage = bg;
+          document.body.style.backgroundSize = "cover";
+
+      }, false);
