00e9f787b6bf2139747014e38e54dbead9d890a9
Sven <sven@hornung.email>
2019-03-01 14:03:48 +0100

    Suche und js angepasst

--- Code/js/suche.js
+++ Code/js/suche.js
@@ -19,3 +19,24 @@ function showTextAndJpg() {
         document.getElementById("home-tab").innerHTML = search.charAt(0).toUpperCase() + search.slice(1);
     }
 }
+
+// Weiterleitung auf die Seite Tagesplanung
+function tagesplanungSeite(){
+    search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 8));
+    var newURL = "tagesplanung.html?search=" + search;
+    document.location.href = newURL;
+}
+
+// Weiterleitung auf die Seite Dream Day
+function dreamdaySeite(){
+    search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 8));
+    var newURL = "dreamday.html?search=" + search;
+    document.location.href = newURL;
+}
+
+// Weiterleitung auf die Seite Eintrag hinzufügen
+function eintragSeite(){
+    search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 8));
+    var newURL = "add.html?search=" + search;
+    document.location.href = newURL;
+}
