bf340552159572eb4936d252cf8590160f9d7929
Sven <sven@hornung.email>
2019-03-01 14:32:02 +0100

    Funktinalität ergänzt  zur Seite Eintrag hinzufüen

--- Code/js/add.js
+++ Code/js/add.js
@@ -1,4 +1,26 @@
+let search;
+let cities = ['amsterdam', 'dubai', 'new york', 'rom', 'paris'];
 
+function start() {
+    search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 8));
+    if (cities.includes(search.toLowerCase())) {
+        showTextAndJpg()
+    } else {
+
+    }
+}
+
+function showTextAndJpg() {
+    document.body.style.backgroundImage = "url(\'" + "img/bg/" + search.replace(' ', '') + ".jpg" + "\')";
+    document.body.style.backgroundSize = "cover";
+    if (search === "new york") {
+        document.getElementById("home-tab").innerHTML = "New York";
+    } else {
+        document.getElementById("home-tab").innerHTML = "Eintrag für " + search.charAt(0).toUpperCase() + search.slice(1) + " hinzufügen";
+    }
+}
+
+// Funktion für das Bild hochladen mit Vorschau
 function previewFile(){
     var preview = document.querySelector(".imgSelector"); //selects the query named img
     var file    = document.querySelector('input[type=file]').files[0]; //sames as here
