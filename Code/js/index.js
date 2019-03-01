e42c8172ba5904cf074a1dd1e0b660f00b1cdd73
Daniel Wenzl <daniel.wenzl@googlemail.com>
2019-03-01 13:52:56 +0100

    Button Trigger
    Button Trigger

--- Code/js/index.js
+++ Code/js/index.js
@@ -1,23 +1,13 @@
-// Initialize Firebase
-/*
-var config = {
-    apiKey: "AIzaSyAlsu8WQwzfgwHy8k--CnnaXO2xYZ-B0yc",
-    authDomain: "urban-explorer-f661d.firebaseapp.com",
-    databaseURL: "https://urban-explorer-f661d.firebaseio.com",
-    projectId: "urban-explorer-f661d",
-    storageBucket: "urban-explorer-f661d.appspot.com",
-    messagingSenderId: "683954933321"
-};
-
-    firebase.initializeApp(config);
-    var db = firebase.firestore();
-
-      }, false);
-*/
-
 function suche(){
     console.log("js");
     var sucheingabe = document.getElementById("suchBar").value;
     var newURL = "suche.html?search=" + sucheingabe;
     document.location.href = newURL;
 }
+
+document.getElementById("suchBar").addEventListener("keyup",function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("suchButton").click();
+    }
+});
