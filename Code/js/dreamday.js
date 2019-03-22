bd09d8e2b9f0d989956659aa0cf7d432d1e2238d
Daniel Wenzl <daniel.wenzl@googlemail.com>
2019-03-22 13:03:26 +0100

    Aktueller Stand DreamDay
    Aktueller Stand DreamDay

--- Code/js/dreamday.js
+++ Code/js/dreamday.js
@@ -4,6 +4,7 @@ let neuesHtml = '';
 search = decodeURIComponent(window.location.href.slice(window.location.href.indexOf('?') + 8));
 let stadtName = search.replace(/ /g,'');
 stadtName = stadtName.toLowerCase();
+let prüfungDoppelt = [];
 
 function start() {
     document.body.style.backgroundImage = "url(\'" + "img/bg/" + search.replace(' ', '') + ".jpg" + "\')";
@@ -27,6 +28,7 @@ function start() {
         });
         dreamdayArray.sort(compare);
         dreamdayArray.forEach(function (element) {
+            prüfungDoppelt.push(element);
             writeHtml(element);
         });
         dreamdayArray.forEach(function (element) {
@@ -56,13 +58,12 @@ function writeHtml(element) {
         + '<li>' + "Adresse: " + element.adresse + '</li>'
         + '<li>' + "Preise: " + element.preise + '</li>'
         + '<li>' + "Öffnungszeiten: " + element.oeffnungszeiten + '</li>'
-        + '<button onclick="austausch('+counter+')";>' + ",," + element.name + "''" +" austauschen" + '</button>'
+        + '<button onclick="austausch(\'' + counter + '\',\'' + element.name + '\')";>' + ",," + element.name + "''" +" austauschen" + '</button>'
         + '</ul>'
         + '</div>'
         + '</div>';
     counter++
 }
-
 function compare(objectA, objectB) {
     if (objectA.dreamday < objectB.dreamday)
         return -1;
@@ -74,11 +75,12 @@ function compare(objectA, objectB) {
 function drucken() {
     window.print();
 }
-function austausch(stelleAuslöser){
+function austausch(stelleAuslöser, name){
     let neueEinträge = [];
     var min = 0;
+    let nameAltesElement = name;
     switch (stelleAuslöser) {
-        case 1:
+        case "1":
             auslöser = 1;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/restaurants").once('value', function(snapshot){
@@ -88,11 +90,46 @@ function austausch(stelleAuslöser){
                     }
                 })
              });
-             var max = neueEinträge.length;
+            for(let b=0; b<neueEinträge.length;b++){
+                 if(neueEinträge[b].name == nameAltesElement){
+                     neueEinträge.splice(b,1);
+                     break;
+                 }
+             }
+             for(let b=0; b<prüfungDoppelt.length;b++){
+                  if(prüfungDoppelt[b].name == nameAltesElement){
+                      prüfungDoppelt.splice(b,1);
+                      break;
+                  }
+              }
+             let neueAnzeige = arrayZusammenführer(neueEinträge, prüfungDoppelt, nameAltesElement);
+             var max = neueAnzeige.length;
              var x = Math.floor(Math.random() * (max - min)) + min;
-             neuesHtmlSchreiben(neueEinträge[x],auslöser);
+             prüfungDoppelt.push(neueAnzeige[x]);
+             neuesHtmlSchreiben(neueAnzeige[x],auslöser);
+
+             /*
+             prüfungDoppelt.forEach(function (element) {
+                    if (neueEinträge[x].name !== element){
+                        neuesHtmlSchreiben(neueEinträge[x],auslöser);
+                        var index = prüfungDoppelt.indexOf(element.name);
+                        console.log(index);
+                        //prüfungDoppelt.splice(index,1);
+                        //prüfungDoppelt.push(neueEinträge[x].name);
+                        console.log(prüfungDoppelt);
+                        return;
+                    }
+                    else {
+
+                        x = Math.floor(Math.random() * (max - min)) + min;
+                        neuesHtmlSchreiben(neueEinträge[x],auslöser);
+                        prüfungDoppelt.push(neueEinträge[x].name);
+
+                    }
+             });
+             */
              //Bild ersetzen
-             let name = neueEinträge[x].name.replace(/ /g,'');;
+             let name = neueEinträge[x].name.replace(/ /g,'');
              sPath = name;
              sPath = sPath.toLowerCase();
              sPath = sPath +'.jpg';
@@ -101,7 +138,7 @@ function austausch(stelleAuslöser){
                  });
              document.getElementById(1).innerHTML = neuesHtml;
             break;
-        case 2:
+        case "2":
             auslöser = 2;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/sehenswuerdigkeiten").once('value', function(snapshot){
@@ -124,7 +161,7 @@ function austausch(stelleAuslöser){
              });
              document.getElementById(2).innerHTML = neuesHtml;
             break;
-        case 3:
+        case "3":
             auslöser = 3;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/restaurants").once('value', function(snapshot){
@@ -147,7 +184,7 @@ function austausch(stelleAuslöser){
              });
              document.getElementById(3).innerHTML = neuesHtml;
             break;
-        case 4:
+        case "4":
             auslöser = 4;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/sehenswuerdigkeiten").once('value', function(snapshot){
@@ -170,7 +207,7 @@ function austausch(stelleAuslöser){
             });
             document.getElementById(4).innerHTML = neuesHtml;
             break;
-        case 5:
+        case "5":
             auslöser = 5;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName + "/sehenswuerdigkeiten").once('value', function(snapshot){
@@ -193,7 +230,7 @@ function austausch(stelleAuslöser){
             });
             document.getElementById(5).innerHTML = neuesHtml;
             break;
-        case 6:
+        case "6":
             auslöser = 6;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/restaurants").once('value', function(snapshot){
@@ -216,7 +253,7 @@ function austausch(stelleAuslöser){
             });
             document.getElementById(6).innerHTML = neuesHtml;
             break;
-        case 7:
+        case "7":
             auslöser = 7;
             //Datenbank Restaurants auslesen
             firebase.database().ref("staedte/" + stadtName +"/bars").once('value', function(snapshot){
@@ -247,13 +284,81 @@ function neuesHtmlSchreiben(element,auslöser){
         + '<div class="liste">'
         + "<img id="+name+" src='' width=300 height=180/>"
         + "<ul>"
-        + '<b>' + element.name + '</b>' 
+        + '<b>' + element.name + '</b>'
         + '<li>' + "Informationen: " + element.informationen + '</li>'
         + '<li>' + "Adresse: " + element.adresse + '</li>'
         + '<li>' + "Preise: " + element.preise + '</li>'
         + '<li>' + "Öffnungszeiten: " + element.oeffnungszeiten + '</li>'
-        + '<button onclick="austausch('+auslöser+')";>' + ",," + element.name + "''" +" austauschen" + '</button>'
+        + '<button onclick="austausch(\'' + auslöser + '\',\'' + element.name + '\')";>' + ",," + element.name + "''" +" austauschen" + '</button>'
         + '</ul>'
         + '</div>'
         + '</div>';
 }
+
+function arrayZusammenführer(neueEinträge, prüfungDoppelt, nameAltesElement){
+    let test = new Set();
+    let duplicates = [];
+    for (var i = 0; i < prüfungDoppelt.length; i++) {
+        for (var j= 0; j < neueEinträge.length; j++) {
+            if(prüfungDoppelt[i].name === neueEinträge[j].name){
+
+                duplicates.push(neueEinträge[j])
+            }
+            else{
+                test.add(neueEinträge[j]);
+            }
+        }
+    }
+    let aEntries = Array.from(test);
+    let arrayDub =[];
+    for(let k =0; k<aEntries.length;k++){
+        arrayDub.push(aEntries[k]);
+    }
+
+    for(let k =0; k<duplicates.length;k++){
+        var index = aEntries.indexOf(duplicates[k]);
+        if(index >=0){
+            aEntries.splice(index,1);
+        }
+    }
+
+
+    return aEntries;
+
+
+/*    for(let b=0; b<prüfungDoppelt.length;b++){
+         if(prüfungDoppelt[b].name == nameAltesElement){
+             prüfungDoppelt.splice(b,1);
+             break;
+         }
+     }
+
+    //Element aus dem einem Array in ein neues schreiben, falls es im anderen nicht vorhanden ist
+    let neueAuswahl = new Set();
+    for (var c = 0; c < neueEinträge.length; c++) {
+        for (var d = 0; d < prüfungDoppelt.length; d++) {
+            if(neueEinträge[c].name !== prüfungDoppelt[d].name)
+                neueAuswahl.add(neueEinträge[c]);
+        }
+    }
+    let test = Array.from(neueAuswahl);
+    return Array.from(neueAuswahl);*/
+
+}
+    /*
+    let arrayDub =[];
+    for(let k =0; k<arrayPossible.length;k++){
+        arrayDub.push(arrayPossible[k]);
+    }
+    for (var i = 0; i < arrayPossible.length; i++) {
+        for (var j = 0; j < arrayShown.length; j++) {
+            if (arrayPossible[i].name===arrayShown[j].name) {
+                var index = arrayDub.indexOf(arrayPossible[i]);
+                if(index>0){
+                    arrayDub.splice(index,1);
+                }
+            }
+        }
+    }
+    return arrayDub;
+    */
